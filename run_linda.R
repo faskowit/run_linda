fd36f209b2671b812ecb8118e42049d57e14cc93
Josh Faskowitz <faskowit@users.noreply.github.com>
2019-12-07 18:47:07 -0500

    Create run_linda.R

---
+++ run_linda.R
@@ -0,0 +1,28 @@
+# Rscript to run LINDA
+
+# load up the package
+library(LINDA)
+
+# command line args
+cmdLineArgs <- commandArgs(trailingOnly = TRUE)
+
+# read command line args
+inputImg <- cmdLineArgs[1]
+outDir <- cmdLineArgs[2]
+
+# check inputs
+if (!file.exists(inputImg)) {
+  stop("input img does not seem to exist")
+}
+
+# create oDir if if does not exist
+dir.create(file.path(oDir))
+# and if couldn't write, stop
+if (!dir.exists(file.path(outDir))) {
+  stop("output dir could not be written")
+}
+
+# runit
+linda_predict(inputImg, outdir = outDir)
+
+# check results in outDir
